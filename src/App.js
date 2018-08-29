ed863d36732d26178a85c59015bddd118f48101e
Aghai Ofir <oaghai@verint.com>
2018-08-29 23:39:55 +0300

    add Provider tag in the root top of the app, and attach store attribute

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import React, { Component } from 'react';
 import User from './components/User'
 import './App.css';
 import Counter from './components/Counter';
-import store from './store'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Counter store={store} />
-        <User store = {store} />
+        <Counter/>
+        <User/>
       </div>
     );
   }
